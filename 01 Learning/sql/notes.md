42d6813c1fd30ea24173d459a05d766bfae4c0e5
Tetricia805 <tetriciag@gmail.com>
2026-04-25 07:29:33 +0300

    feat: initialize retail shop database schema Created tables for goods, customers, retailers, and orders. Implemented primary and foreign key relationships. Added order_items junction table to handle multiple goods per order. Refined data types for prices and phone numbers.

--- 01 Learning/sql/notes.md
+++ 01 Learning/sql/notes.md
@@ -9,3 +9,14 @@ The Database: The shelves where everything is organized.
 The DBMS (Database Management System): This is the "Librarian." E.g  MySQL. It’s the software that lets you ask for specific information, add new data, or delete old stuff.
 
 
+It is comprise of entities, attributes and relationships
+
+The Entity (The "Thing")
+An entity is a person, place, object, or event that you want to store data about. In a database, an entity usually becomes a Table.
+
+Examples: A Student, a Product, a Car, or a Sale
+
+The Attribute (The "Detail")
+An attribute is a specific piece of information that describes the entity. In a database, attributes become the Columns of your table.
+Examples: If the entity is a Student, the attributes are Name, Date of Birth, and Email.
+
---
+++ 01 Learning/sql/practice_1.sql
@@ -0,0 +1,55 @@
+
+DROP DATABASE IF EXISTS "small_retail_shop";
+
+CREATE DATABASE "small_reatil_shop";
+
+USE "small_retail_shop";
+
+CREATE TABLE goods (
+    good_id INT NOT NULL PRIMARY KEY,
+    good_name VARCHAR(50),
+    expiry_date DATE,
+    stock_date DATETIME,
+    good_pice DECIMAL(10,2) 
+);
+
+
+CRAETE TABLE customer(
+    customer_id INT NOT NULL PRIMARY KEY,
+    customer_name VARCHAR(100),
+    customer_email VARCHAR(50),
+    customer_phone_numebr VARCHAR(20),
+
+);
+
+
+CREATE TABLE retailer( 
+    retailer_id INT NOT NULLL PRIMARY KEY,
+    retailer_name VARCHAR(100),
+    retailer_phone_number VARCHAR(20),
+    retailer_email VARCHAR(50)
+
+
+)
+
+CREATE TABLE orders(
+    order_id INT NOT NULL PRIMARY KEY,
+    oredr_date DATETIME,
+    order_amount DECIMAL,
+    customer_id
+    FOREIGN KEY (customer_id) REEFENCES customer(customer_id)
+)
+
+
+
+-- This is the "Bridge Table" (The Receipt Lines)
+-- It links  Orders to Goods
+CREATE TABLE order_items (
+    order_item_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    order_id INT, -- Links to the Orders table
+    good_id INT,  -- Links to the Goods table
+    quantity INT, -- How many did they buy?
+    price_at_sale DECIMAL(10,2), -- Price changes
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (good_id) REFERENCES goods(good_id)
+);
