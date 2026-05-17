77d5b3d7b53bddd75d3748a1dab1c25982ed9645
Najjuma Teopista <tetriciag@gmail.com>
2026-05-17 22:42:39 +0300

    Create leetcode_practices.sql with customer query
    Add SQL query to find customers based on referral criteria.

---
+++ 01 Learning/sql/leetcode_practices.sql
@@ -0,0 +1,10 @@
+
+-- Find the names of the customer that are either:
+-- referred by any customer with id != 2.
+-- not referred by any customer.
+
+
+SELECT
+name 
+FROM Customer
+WHERE referee_id != 2 OR referee_id IS NULL;
