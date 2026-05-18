d75d060812859420bce087b6f91c3c914adeb5a4
Najjuma Teopista <tetriciag@gmail.com>
2026-05-18 09:02:50 +0300

    used select statement with the >= operators
    Added a query to find names, population, and area of big countries.

--- 01 Learning/sql/leetcode_practices.sql
+++ 01 Learning/sql/leetcode_practices.sql
@@ -3,8 +3,15 @@
 -- referred by any customer with id != 2.
 -- not referred by any customer.
 
-
 SELECT
 name 
 FROM Customer
 WHERE referee_id != 2 OR referee_id IS NULL;
+
+-- Write your MySQL query statement below
+-- Write a solution to find the name, population, and area of the big countries
+
+SELECT name, population, area
+FROM World
+WHERE
+area >= 3000000 OR population >= 25000000;
