1acc808416203d055a7f45ab04197fe3e24cd9c4
Najjuma Teopista <tetriciag@gmail.com>
2026-05-20 18:02:14 +0300

    Implement query to identify invalid tweets
    Add SQL query to find invalid tweets based on content length.

--- 01 Learning/sql/leetcode_practices.sql
+++ 01 Learning/sql/leetcode_practices.sql
@@ -34,3 +34,15 @@ WHERE author_id = viewer_id
 GROUP BY author_id, viewer_id
 HAVING count(*) >= 1
 ORDER BY author_id ASC;
+
+-- Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+
+-- Return the result table in any order.
+
+SELECT
+    tweet_id
+FROM 
+    Tweets 
+WHERE
+    CHAR_LENGTH(content) > 15;
+
