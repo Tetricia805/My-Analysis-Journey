d86d641b13d788e80c34408d03387351ea7458be
Najjuma Teopista <tetriciag@gmail.com>
2026-05-25 17:15:57 +0300

    Add SQL query for product sales analysis and used an inner join
    Added SQL query for product sales analysis.

--- 01 Learning/sql/leetcode_practices.sql
+++ 01 Learning/sql/leetcode_practices.sql
@@ -58,4 +58,15 @@ RIGHT JOIN Employees e
 ON 
 ui.id = e.id;
 
+-- 1068. Product Sales Analysis
+-- Write a solution to report the product_name, year, and price for each sale_id in the Sales table.
 
+-- Return the resulting table in any order.
+SELECT
+    p.product_name,s.year, s.price
+FROM 
+    Sales s
+JOIN 
+    Product p
+ON 
+s.product_id = p.product_id;
