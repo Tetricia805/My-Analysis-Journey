9ea2e051c54e1e9d87d7e1e7b15305a7acea376f
Najjuma Teopista <tetriciag@gmail.com>
2026-05-29 21:29:32 +0300

    Add query for rising temperature problem
    Add SQL query for finding dates with rising temperatures.

--- 01 Learning/sql/leetcode_practices.sql
+++ 01 Learning/sql/leetcode_practices.sql
@@ -70,3 +70,18 @@ JOIN
     Product p
 ON 
 s.product_id = p.product_id;
+
+-- 197.Rising Temperature
+-- Write a solution to find all dates' id with higher temperatures compared to its previous dates (yesterday)
+
+
+SELECT 
+    w1.id
+FROM 
+    Weather w1
+JOIN 
+    Weather w2
+ON 
+    DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE 
+    w1.temperature > w2.temperature;
